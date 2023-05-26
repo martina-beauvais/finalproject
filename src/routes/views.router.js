395ba1344b4b29daf9c32c87b94572a300b3408f
Martina Beauvais <marbeauvais17@gmail.com>
2023-05-26 16:12:17 -0300

    second commit

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -4,11 +4,11 @@ import viewsController from "../controllers/views.controller.js";
 
 const router = Router();
 
+router.get('/',viewsController.login);
 router.get('/home', executePolicies(["USER"]), viewsController.home);
 router.get('/products', executePolicies(["USER"]),viewsController.products);
 router.get('/contact',executePolicies(["USER"]), viewsController.contact);
 router.get('/register',viewsController.register);
-router.get('/login',viewsController.login);
 router.get('/profile', executePolicies(["USER"]), viewsController.profile)
 router.get('/loginFail', viewsController.failLogin)
 router.get('/cart', executePolicies(["USER"]),viewsController.cart);
